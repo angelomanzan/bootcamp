a3334b86c45a7b859aeaa1661db9080956f6036f
root <root@ip-172-31-9-75.sa-east-1.compute.internal>
2020-12-18 01:45:46 +0000

    codigo fonte

---
+++ rds.conf.php
@@ -0,0 +1,1 @@
+<?php $RDS_URL='bootcamp.cig3fmv0mdrq.sa-east-1.rds.amazonaws.com'; $RDS_DB='dados'; $RDS_user='admin'; $RDS_pwd='jaumdomo9'; ?>
---
+++ s3-conf.php
@@ -0,0 +1,9 @@
+<?php
+$accessKey = 'AKIAVFSEKBNM3C7Q77WT';
+$secretKey = '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';
+$region = 'sa-east-1';
+$bucket = 'bootcamp4.cloud';
+$arqName =  'logo.jpg';
+$linkestatico = 'http://bootcamp4.cloud.s3-website-sa-east-1.amazonaws.com'
+?>
+
